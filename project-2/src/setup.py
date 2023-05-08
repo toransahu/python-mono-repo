aa242c5bb7ea1410ad6868b624a96adc81789854
Toran Sahu <toran.sahu@yahoo.com>
2023-05-08 17:11:55 +0530

    Add project-2

---
+++ project-2/src/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+
+# get the dependencies and installs
+with open("requirements.txt", encoding="utf-8") as f:
+    requires = []
+    for line in f:
+        req = line.split("#", 1)[0].strip()
+        if req and not req.startswith("--"):
+            requires.append(req)
+
+setup(
+    name="package2",
+    version="0.1",
+    packages=find_packages(include=["package2"]),
+    install_requires=requires,
+    extras_require={},
+)
